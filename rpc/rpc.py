e782c465976b43c5ba7af24740b1c7d9ee0e3e06
Rakan Al-Qaqaa <rakan@nyu.edu>
2016-12-02 18:41:59 -0500

    Added rpc

---
+++ rpc/rpc.py
@@ -0,0 +1,27 @@
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCRequestHandler
+import numpy as np
+import cv2
+from PIL import Image
+import urllib, cStringIO
+
+def processFaces(URL):
+    img = Image.open(cStringIO.StringIO(urllib.urlopen(URL).read()))
+    cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    if(img is None):
+        return "Image did not load"
+    else:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        return cascade.detectMultiScale(
+                gray,
+                scaleFactor = 1.15,
+                minNeighbors = 5,
+                minSize = (30, 30),
+                flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+              )
+
+
+server = SimpleXMLRPCServer(("0.0.0.0", 80))
+server.register_function(processFaces, 'face')
+print('Serving XML-RPC on localhost port 8000')
+server.serve_forever()
