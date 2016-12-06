694613cdda873f6651050679f58cad0f581f7da6
Rakan Al-Qaqaa <rakan@nyu.edu>
2016-12-06 12:40:21 -0500

    Testing face detection

--- web/scalica/micro/views.py
+++ web/scalica/micro/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.core.cache import cache
 
