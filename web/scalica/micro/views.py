26b081bf75ee931600aad9cd9dd959fc9486126c
Rakan Al-Qaqaa <rakan@nyu.edu>
2016-12-18 22:02:33 -0500

    minor bugfix

--- web/scalica/micro/views.py
+++ web/scalica/micro/views.py
@@ -135,7 +135,15 @@ def image(request, image_id):
         curr_user = request.user
         faces_in_photo = image.tags.all()
         tagged_users = [o.user for o in image.tags.all() if not o.user == None]
-
+        scaleFactor = 600/max(image.image.height, image.image.width)
+        if(scaleFactor < 1):
+            for i in range(0, len(faces_in_photo)):
+                thisFace = faces_in_photo[i]
+                thisFace.x *= scaleFactor
+                thisFace.y *= scaleFactor
+                thisFace.height *= scaleFactor
+                thisFace.width *= scaleFactor
+                faces_in_photo[i] = thisFace
         context = {
             'my_photo': my_photo,
             'image_url': image_url,
