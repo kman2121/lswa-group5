05c5d58e717e9293f3385b1dce271cb4dda4a14f
Rakan Al-Qaqaa <rakan@nyu.edu>
2016-12-21 19:15:28 -0500

    Added tag api

--- web/scalica/micro/static/micro/javascripts/tagimage.js
+++ web/scalica/micro/static/micro/javascripts/tagimage.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function(event){
   }
   tgpg.appendChild(prompt);
   document.getElementById("yesBtn").addEventListener('click', generateTag);
+  getTags();
 });
 
 
